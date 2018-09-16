f307f92d935bc5e589b7a0c3d64f3452d9cdd60e
Pavel Pavlovskiy <pavlovskiypavel1989@gmail.com>
2018-09-16 23:11:15 +0700

    worked with app menu and saving session data settings
    done moving menu, logic to save data in sessionStorage and logic of toggled menu buttons

--- js/solution.js
+++ js/solution.js
@@ -3,30 +3,47 @@ function initApp() {
 	const [app] = document.getElementsByClassName('app'),
 				[menu] = app.getElementsByClassName('menu'),
 				[newImgBtn] = menu.getElementsByClassName('new'),
+				[commentsBtn] = menu.getElementsByClassName('comments'),
+				[commentsTools] = menu.getElementsByClassName('comments-tools'),
+				[drawBtn] = menu.getElementsByClassName('draw'),
+				[drawTools] = menu.getElementsByClassName('draw-tools'),
+				[shareBtn] = menu.getElementsByClassName('share'),
+				[shareTools] = menu.getElementsByClassName('share-tools'),
         [image] = app.getElementsByClassName('current-image'),
         [preloader] = app.getElementsByClassName('image-loader'),
         [errorMsg] = app.getElementsByClassName('error'),
         [errorHeader] = errorMsg.getElementsByClassName('error__header'),
       	[errorText] = errorMsg.getElementsByClassName('error__message');
+
+  const burgerBtn = menu.removeChild(menu.getElementsByClassName('burger')[0]),
+  			commentsForm = app.removeChild(app.getElementsByClassName('comments__form')[0]);   	
   
-  const apiURL = 'https://neto-api.herokuapp.com/pic';   
+  const apiURL = 'https://neto-api.herokuapp.com/pic';
 
-  ////////////////////////////////////////////////////////////////////
-  const burger = menu.removeChild(menu.getElementsByClassName('burger')[0]),
-  			commentsForm = app.removeChild(app.getElementsByClassName('comments__form')[0]);
-  (() => menu.dataset.state = 'initial')();
-  const imageSettings = (() => {
+  /////////////////////////////////////////////////////////////////
+
+  const throttle = (cb) => {
+    let isWaiting = false;
+    return function (...args) {
+      if (!isWaiting) {
+        cb.apply(this, args);
+        isWaiting = true;
+        requestAnimationFrame(() => isWaiting = false);
+      }
+    }
+  };
+
+  const getSessionSettings = ( key ) => {
   	try {
-	    if (sessionStorage.imageSettings) { 
-	      return JSON.parse(sessionStorage.imageSettings);
-	    }
-	  } catch (err) {
-	    console.error(`${err}`);
-	  }
-  })();
-  image.src = imageSettings ? imageSettings.url : '';
+			if (sessionStorage[key]) { 
+				return JSON.parse(sessionStorage[key]);
+			}
+		} catch (err) {
+			console.error(`${err}`);
+		}
+	};
 
-  ////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
 
   const showElement = ( el ) => {
     el.style.display = '';
@@ -35,6 +52,66 @@ function initApp() {
   const hideElement = ( el ) => {
     el.style.display = 'none';
   };
+
+  const selectMenuModeTo = ( mode, selectedItemType ) => {
+  	switch(mode) {
+		  case 'initial':
+		    menu.dataset.state = 'initial';
+		    break;
+
+		  case 'default':
+		    menu.dataset.state = 'default';
+		    Array.from(menu.querySelectorAll(`[data-state='selected']`)).forEach(el => el.dataset.state = '');
+		    break;
+
+		  case 'selected':
+		    menu.dataset.state = 'selected';
+		    [commentsBtn, drawBtn, shareBtn].find(
+		  		btn => btn.classList.contains(selectedItemType)
+		  	).dataset.state = 'selected';
+		  	[commentsTools, drawTools, shareTools].find(
+		  		tools => tools.classList.contains(selectedItemType + '-tools')
+		  	).dataset.state = 'selected';
+		    break;
+		}
+
+		sessionStorage.menuStateSettings = JSON.stringify( {mode: mode, selectItemType: selectedItemType} );
+	};
+
+  const selectMenuMode = event => {
+  	if (burgerBtn === event.target || burgerBtn === event.target.parentElement) {
+  		selectMenuModeTo('default');
+  	} else if (drawBtn === event.target || drawBtn === event.target.parentElement) {
+  		selectMenuModeTo('selected', 'draw');	
+  	} else if (commentsBtn === event.target || commentsBtn === event.target.parentElement) {
+  		selectMenuModeTo('selected', 'comments');
+  	} else if (shareBtn === event.target || shareBtn === event.target.parentElement) {
+  		selectMenuModeTo('selected', 'share');
+  	} 
+  }	
+
+	const renderApp = () => {
+  	const imageSettings = getSessionSettings('imageSettings'),
+					menuStateSettings = getSessionSettings('menuStateSettings'),
+					menuPositionSettings = getSessionSettings('menuPositionSettings');
+
+		image.src = imageSettings ? imageSettings.url : '';
+
+	  if (menuStateSettings) {
+	  	selectMenuModeTo(menuStateSettings.mode, menuStateSettings.selectItemType);
+	  } else {
+	  	selectMenuModeTo('initial');
+	  }
+
+	  if (menuPositionSettings) {
+	  	menu.style.left = menuPositionSettings.left + 'px';
+			menu.style.top = menuPositionSettings.top + 'px';
+	  } 
+  };	
+
+  renderApp();
+
+  //////////////////////////////////////////////////////////////////
 
   const checkResponseStatus = ( resp ) => {
   	if (200 <= resp.status && resp.status < 300) {
@@ -52,16 +129,16 @@ function initApp() {
 	  showElement(errorMsg);
   }
 
+  const showImage = ( imgData ) => {
+		image.src = imgData.url;
+		image.dataset.status = 'load';
+		sessionStorage.imageSettings = JSON.stringify(imgData);
+ 	};
+
   const loadImage = ( {id} ) => {
   	fetch(apiURL + '/' + id)
   	.then(checkResponseStatus)
-		.then(imgData => {
-			image.addEventListener('load', () => hideElement(preloader));
-
-			image.src = imgData.url;
-			image.dataset.status = 'load';
-			sessionStorage.imageSettings = JSON.stringify(imgData);
-		})
+		.then(showImage)
 		.catch(err => postError(errorHeader.textContent, err.message));
   };
 
@@ -114,10 +191,76 @@ function initApp() {
     }
   };
 
+  /////////////////////////////////////////////////////////
+
+  let dragged = null,
+  		draggedSettings = null;
+
+	const putMenu = event => {
+		event.preventDefault();
+		if (event.target.classList.contains('drag')) {
+			dragged = event.target.parentElement;
+			
+			const draggedBounds = event.target.getBoundingClientRect(),
+			      draggedCSS = getComputedStyle(dragged);
+
+			draggedSettings = { 
+				shiftX: draggedBounds.width / 2,
+				shiftY: draggedBounds.height / 2,
+				minX: app.offsetLeft,
+				maxX: app.offsetWidth - Number(draggedCSS.width.replace('px', '')),
+				minY: app.offsetTop,
+				maxY: app.offsetHeight - Number(draggedCSS.height.replace('px', ''))
+			};
+		}
+	}
+
+	const dragMenu = (pageX, pageY) => {
+		if (dragged) {
+	    event.preventDefault();
+	    let X = pageX - draggedSettings.shiftX,
+	    		Y = pageY - draggedSettings.shiftY;
+
+	    X = Math.min(X, draggedSettings.maxX);
+	    Y = Math.min(Y, draggedSettings.maxY);
+	    X = Math.max(X, draggedSettings.minX);
+	    Y = Math.max(Y, draggedSettings.minY);
+		
+	   	dragged.style.left = X + 'px';
+			dragged.style.top = Y + 'px';
+			dragged.style.pointerEvents = 'none';
+		}
+	}
+
+	const dropMenu = event => {
+	  if (dragged) { 
+	  	dragged.style.pointerEvents = '';
+	  	sessionStorage.menuPositionSettings = JSON.stringify( {left: dragged.offsetLeft, top: dragged.offsetTop} );
+	  	dragged = null; 
+	  }
+	}
+
+	/////////////////////////////////////////////////////////////////////////////
+	
   //Загрузка файла на сервер:
 	menu.addEventListener('click', uploadNewByInput);
   app.addEventListener('dragover', event => event.preventDefault());
   app.addEventListener('drop', uploadNewByDrop);
+  image.addEventListener('load', () => {
+  	hideElement(preloader);
+		menu.insertBefore(burgerBtn, newImgBtn);
+		selectMenuModeTo('selected', 'share');
+  });
+
+	//Перемещение меню:
+	const moveMenu = throttle( (...coords) => dragMenu(...coords) );
+
+  document.addEventListener('mousedown', putMenu);
+	document.addEventListener('mousemove', event => moveMenu(event.pageX, event.pageY));
+	document.addEventListener('mouseup', dropMenu);
+
+	//Переключение пунктов меню:
+	menu.addEventListener('click', selectMenuMode);
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
