b548a269e15add8277111b3b550f7007a29bd7c8
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-10-02 23:55:58 +0700

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,15 @@
-﻿/******************** Плавная отрисовка линий при рисовании и коррекция положения меню по ширине *****************/
+/******************** Плавная отрисовка линий при рисовании и коррекция положения меню по ширине *****************/
+const tick = () => {
+  if (needsRendering) {
+    draw(ctx);
+    needsRendering = false;
+  }
 
-  const tick = () => {
-    if (needsRendering) {
-      draw(ctx);
-      needsRendering = false;
-    }
-	  
-    let crntMenuLeftPos = menu.getBoundingClientRect().left;
-    while (menu.offsetHeight > defaultMenuHeight) {
-      menu.style.left = (--crntMenuLeftPos) + "px";
-    } 
-	  
-    window.requestAnimationFrame(tick);
-  };
-  tick();
+  let crntMenuLeftPos = menu.getBoundingClientRect().left;
+  while (menu.offsetHeight > defaultMenuHeight) {
+    menu.style.left = (--crntMenuLeftPos) + "px";
+  } 
+
+  window.requestAnimationFrame(tick);
+};
+tick();
