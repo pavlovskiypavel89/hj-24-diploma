01b027d41f0798f65586839d49876378b7527f10
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-10-06 14:36:26 +0700

    Update comments.js

--- js/comments.js
+++ js/comments.js
@@ -70,7 +70,6 @@ const crtNewCommentsForm = (left, top) => {
 };
 
 const parseNewCommentsForm = comment => {
-  console.log(2);
   if (!picture.firstElementChild) {
     return document.createTextNode("");
   }
